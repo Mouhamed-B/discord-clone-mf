37c770355c5cee5c82a640ce4b9948d527bf07c6
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-02-13 22:27:04 +0000

    setup:add channels app

--- apps/landing/src/app/app.routes.ts
+++ apps/landing/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'channels',
+    loadChildren: () => import('channels/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
